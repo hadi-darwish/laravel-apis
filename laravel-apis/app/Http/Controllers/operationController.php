2864c4b2b5bf1b0d5ad83aecfd3e92ef4f61a85a
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-09-30 23:42:37 +0300

    adding operation method of operations api

--- laravel-apis/app/Http/Controllers/operationController.php
+++ laravel-apis/app/Http/Controllers/operationController.php
@@ -6,4 +6,36 @@ use Illuminate\Http\Request;
 
 class operationController extends Controller
 {
+    //this function will solve the operation given by the user
+    function getResult(Request $request)
+    {
+        $text = $request->text;
+        $array = explode(" ", $text);
+        $result = $array[1];
+        //this will check for what kind of operation will be used
+        if ($array[0] == "+") {
+            for ($i = 2; $i < count($array); $i++) {
+                $result += $array[$i];
+            }
+        } else if ($array[0] == "-") {
+            for ($i = 2; $i < count($array); $i++) {
+                $result -= $array[$i];
+            }
+        } else if ($array[0] == "/") {
+            for ($i = 2; $i < count($array); $i++) {
+                $result /= $array[$i];
+            }
+        } else if ($array[0] == "*") {
+            for ($i = 2; $i < count($array); $i++) {
+                $result *= $array[$i];
+            }
+        } else if ($array[0] == "%") {
+            for ($i = 2; $i < count($array); $i++) {
+                $result %= $array[$i];
+            }
+        }
+        return response()->json([
+            "result" => $result
+        ]);
+    }
 }
