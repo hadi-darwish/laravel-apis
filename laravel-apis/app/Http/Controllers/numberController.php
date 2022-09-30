f9d751c9ea786ca64bfc957bce489ce6f3695903
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-09-30 20:30:03 +0300

    adding getArray api

--- laravel-apis/app/Http/Controllers/numberController.php
+++ laravel-apis/app/Http/Controllers/numberController.php
@@ -6,4 +6,23 @@ use Illuminate\Http\Request;
 
 class numberController extends Controller
 {
+    function getArray($number)
+    {
+        //counter will handel at what place value are the number in
+        $counter = 0;
+        $array = [];
+        //this will add the numbers to the array where sum of all numbers will 
+        //be the same number given by the user
+        while ($number != 0) {
+            $array[] =  (($number % 10) * (10 ** $counter));
+            $counter++;
+            $number /= 10;
+            $number = round($number, 0);
+        }
+        //this will reverse the result array
+        $array = array_reverse($array);
+        return response()->json([
+            "result" => $array
+        ]);
+    }
 }
