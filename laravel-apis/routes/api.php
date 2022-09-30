0ef9e8eec3a0fb6b2da4b8421bb6a31c29c172c0
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-09-30 20:30:30 +0300

    adding /num route

--- laravel-apis/routes/api.php
+++ laravel-apis/routes/api.php
@@ -4,6 +4,14 @@ use App\Http\Controllers\numberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+Route::group(['prefix' => "v1"], function () {
+
+    Route::get("/num/{number}", [numberController::class, 'getArray']);
+});
+
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
